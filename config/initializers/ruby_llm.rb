f4bfbbe6aa46d5d9e350dfcb7ae991da77b847fb
Alex Finnarn <alex.finnarn@gmail.com>
2025-08-05 16:21:26 -0400

    add ollama config to ruby_llm initializer

--- config/initializers/ruby_llm.rb
+++ config/initializers/ruby_llm.rb
@@ -1,6 +1,8 @@
 RubyLLM.configure do |config|
-  config.openai_api_key = ENV["OPENAI_API_KEY"]
-  config.anthropic_api_key = ENV["ANTHROPIC_API_KEY"]
+  # config.openai_api_key = ENV["OPENAI_API_KEY"]
+  # config.anthropic_api_key = ENV["ANTHROPIC_API_KEY"]
 
   # config.default_model = "gpt-4.1-nano"
+
+  config.ollama_api_base = "http://localhost:11434"
 end
